dc2cac4c8d898aeeeb4442539827f7348c8db77c
Umar <https://github.com/UmarKamalov/Python.git>
2026-03-09 21:48:47 +0300

    Lesson 1

--- Lisson 1/Lesson 1.1.py
+++ Lisson 1/Lesson 1.1.py
@@ -1,1 +1,15 @@
-print ("Салам мир!!")
+print ("мир!!")
+a = 1
+b = 2
+c = 3
+a, b, c = b, c, a
+print(type (a))
+print (type(b))
+print (type(c))
+a = 7
+b = 6
+c = 5
+print (id(a))
+print (id(b))
+print (id(c))
+
